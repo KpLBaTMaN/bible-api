72fb75249f1132b878cf151832fa433a15e56475
40235515 <kpl_batman123@live.co.uk>
2023-07-01 13:16:39 +0100

    created endpoints with a fronend and api endpoints for the getting by book and by chapter

--- app.py
+++ app.py
@@ -4,90 +4,86 @@ import webbrowser
 import sqlite3
 
 from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import pandas as pd
 
 
 from backend.database import (
-    get_verse,
-    create_verse,
+    retrieve_verse,
+    retrieve_verses,
+    retrieve_book,
     get_random_verse,
     search_verses
 )
 
-app = FastAPI()
+app = FastAPI(
+    title="BibleAPI",
+    description="A Bible API that contains the KJV bible. (In development)",
+    version="0.0.1",
+    contact={
+        "name": "Andrew brown",
+        "url": "https://github.com/KpLBaTMaN/bible-api"
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+)
+
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-
-def populate_database():
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS verses (id INTEGER PRIMARY KEY AUTOINCREMENT, chapter INTEGER, verse INTEGER, text TEXT, translation_id TEXT, book_id TEXT, book_name TEXT)')
-    
-    # populate database 
-    csv_path = "./backend/kjv.csv"
-    df = pd.read_csv(csv_path)
-
-    # Insert the verses into the database
-    for verse in df.to_dict('records'):
-        cursor.execute('INSERT INTO verses (chapter, verse, text, translation_id, book_id, book_name) VALUES (?, ?, ?, ?, ?, ?)',
-                   (verse["chapter"], verse["verse"], verse["text"], verse["translation_id"], verse["book_id"], verse["book_name"]))
-        
-    conn.commit()
-    conn.close()
-    
-populate_database()
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.get("/v1/verses/random")
+# Route for the search page
+@app.get('/search', response_class=HTMLResponse)
+async def search_page(request: Request):
+    return templates.TemplateResponse("search.html", {"request": request})
+
+
+# Route for handling the search request
+@app.post('/search/results', response_class=HTMLResponse)
+async def search(request: Request, keyword: str = Form(...)):
+    verses = search_verses(keyword)  # Call the search_verses function
+    return templates.TemplateResponse("results.html", {"request": request, "verses": verses})
+
+
+@app.get("/v1/verses/random", response_class=JSONResponse)
 def get_random_verse_endpoint():
     # Code to fetch a random Bible verse
     return get_random_verse()
     
-@app.get("/v1/verses")
+@app.get("/v1/verses", response_class=JSONResponse)
 def search_verses_endpoint(keyword: str):
     # Code to search for verses based on a keyword
     return search_verses(keyword)
 
-@app.get("/v1/verses/{book_name}/{chapter}/{verse}")
+@app.get("/v1/verses/{book}/{chapter}/{verse}", response_class=JSONResponse)
 def get_verse_endpoint(book: str, chapter: int, verse: int):
     # Code to retrieve a specific verse by reference
-    return get_verse(book, chapter, verse)
+    return retrieve_verse(book, chapter, verse)
 
-# @app.get("/v1/verses/{book}/{chapter}")
-# def get_verses(book: str, chapter: int):
-#     # Code to retrieve verses from a specific book and chapter
-#     # Return the verses in the desired format
-#     return ""
+@app.get("/v1/verses/{book}/{chapter}", response_class=JSONResponse)
+def get_verses_endpoint(book: str, chapter: int):
+     # Code to retrieve a specific book by reference
+    return retrieve_verses(book, chapter)
 
-# @app.get("/v1/verses/{book}/{chapter}")
-# def get_verses_range(book: str, chapter: int, start: int, end: int):
-#     # Code to retrieve a range of verses within a specific book and chapter
-#     # Return the verses in the desired format
-#     return ""
-    
-
-# @app.post("/verses")
-# async def create_verse_endpoint(verse: dict):
-#     return create_verse(verse)
-
-@app.get("/verses/{verse_id}")
-async def get_verse_endpoint(verse_id: int):
-    return get_verse(verse_id)
-
-
-# # Close the database connection when the application shuts down
-# @app.on_event("shutdown")
-# def close_db_connection():
-#     conn.close()
+@app.get("/v1/books/{book}", response_class=JSONResponse)
+def get_book(book: str):
+    return retrieve_book(book)
 
 
 # Run the FastAPI application using uvicorn
 if __name__ == "__main__":
+    domain = "localhost"
+    
     # Automatically open the application in the browser
-    webbrowser.open("http://localhost:8000/docs")
+    webbrowser.open(f"http://{domain}:8000/docs")
 
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host=domain, port=8000)
--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,34 @@
 
 import sqlite3
 import random
-
+import pandas as pd
 from fastapi import HTTPException
 
+database_file = 'database.db'
+
+
+def populate_database():
+    conn = sqlite3.connect(database_file)
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS verses (id INTEGER PRIMARY KEY AUTOINCREMENT, chapter INTEGER, verse INTEGER, text TEXT, translation_id TEXT, book_id TEXT, book_name TEXT)')
+    
+    # populate database 
+    csv_path = "./data/kjv.csv"
+    df = pd.read_csv(csv_path)
+
+    # Insert the verses into the database
+    for verse in df.to_dict('records'):
+        cursor.execute('INSERT INTO verses (chapter, verse, text, translation_id, book_id, book_name) VALUES (?, ?, ?, ?, ?, ?)',
+                   (verse["chapter"], verse["verse"], verse["text"], verse["translation_id"], verse["book_id"], verse["book_name"]))
+        
+    conn.commit()
+    conn.close()
+    
+
+
+
 def get_random_verse():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect(database_file)
     cursor = conn.cursor()
     
     # Retrieve the total count of verses in the table
@@ -33,7 +56,7 @@ def get_random_verse():
     return verse
 
 def search_verses(keyword: str):
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect(database_file)
     cursor = conn.cursor()
     # Execute the SQL query to search for verses based on the keyword
     cursor.execute("SELECT chapter, verse, text, translation_id, book_id, book_name FROM verses WHERE text LIKE ?", ('%' + keyword + '%',))
@@ -58,7 +81,7 @@ def search_verses(keyword: str):
 
 
 def create_verse(verse: dict):
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect(database_file)
     cursor = conn.cursor()
     cursor.execute('INSERT INTO verses (chapter, verse, text, translation_id, book_id, book_name) VALUES (?, ?, ?, ?, ?, ?)',
                    (verse["chapter"], verse["verse"], verse["text"], verse["translation_id"], verse["book_id"], verse["book_name"]))
@@ -67,15 +90,17 @@ def create_verse(verse: dict):
     return {"message": "Verse created"}
 
 
-def get_verse(book: str, chapter: int, verse: int):
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    
-    # Execute the SQL query to retrieve the specific verse
-    cursor.execute("SELECT text, translation_id, book_id, book_name FROM verses WHERE book_name = ? AND chapter = ? AND verse = ?", (book, chapter, verse))
-    verse_data = cursor.fetchone()
-    
-    
+def retrieve_verse(book: str, chapter: int, verse: int):
+    # Retrieve a specific verse by reference
+    with sqlite3.connect(database_file) as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT text, translation_id, book_id, book_name "
+            "FROM verses WHERE book_name = ? AND chapter = ? AND verse = ?",
+            (book, chapter, verse)
+        )
+        verse_data = cursor.fetchone()
+
     # Check if the verse exists in the database
     if verse_data is None:
         raise HTTPException(status_code=404, detail="Verse not found")
@@ -87,10 +112,70 @@ def get_verse(book: str, chapter: int, verse: int):
         "book_id": verse_data[2],
         "book_name": verse_data[3]
     }
-    conn.commit()
-    conn.close()
+
     return verse
 
+
+def retrieve_verses(book: str, chapter: int):
+    # Establish a connection to the database
+    with sqlite3.connect(database_file) as conn:
+        cursor = conn.cursor()
+
+        # Execute the SQL query to retrieve all verses in the chapter
+        cursor.execute("SELECT text, translation_id, book_id, book_name, verse FROM verses WHERE book_name = ? AND chapter = ?", (book, chapter))
+        verses_data = cursor.fetchall()
+
+        # Check if any verses exist in the chapter
+        if not verses_data:
+            raise HTTPException(status_code=404, detail="No verses found in the chapter")
+
+        # Create a list of verse dictionaries
+        verses = []
+        for verse_data in verses_data:
+            verse = {
+                "text": verse_data[0],
+                "translation_id": verse_data[1],
+                "book_id": verse_data[2],
+                "book_name": verse_data[3],
+                "verse": verse_data[4]
+            }
+            verses.append(verse)
+
+        return verses
+    
+
+
+def retrieve_book(book: str):
+    # Establish a connection to the database
+    with sqlite3.connect(database_file) as conn:
+        cursor = conn.cursor()
+
+        # Execute the SQL query to retrieve all verses in the book
+        cursor.execute(
+            "SELECT text, translation_id, book_id, book_name, chapter, verse "
+            "FROM verses WHERE book_name = ?",
+            (book,)
+        )
+        verses_data = cursor.fetchall()
+
+        # Check if any verses exist in the book
+        if not verses_data:
+            raise HTTPException(status_code=404, detail="No verses found in the book")
+
+        # Create a list of verse dictionaries
+        verses = []
+        for verse_data in verses_data:
+            verse = {
+                "text": verse_data[0],
+                "translation_id": verse_data[1],
+                "book_id": verse_data[2],
+                "book_name": verse_data[3],
+                "chapter": verse_data[4],
+                "verse": verse_data[5]
+            }
+            verses.append(verse)
+
+        return verses
 
 # def get_verse(verse_id: int):
 #     conn = sqlite3.connect('database.db')
